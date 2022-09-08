23a8b8ef75a6acac22f74a2e18b0ef4b7bb2d0e6
Kiur Jullis <kiur.jullis@gmail.com>
2022-09-08 15:41:44 +0300

    Refactor JS

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
+(() => {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", openNavLink));
+  hamburger.addEventListener("click", mobileMenu);
+  navLink.forEach(n => n.addEventListener("click", openNavLink));
 
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
-
-function openNavLink(event) {
-  event.preventDefault();
-  document.getElementsByClassName("nav-link").style.display = "block";
-  /*
-  Display the block element, and add a nested subnav ul element to the DOM
-  */
-
-
-
-
-}
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  const mobileMenu = () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
   }
-}
+
+  const closeMenu = () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+
+  const openNavLink = (e) => {
+    e.preventDefault();
+    document.getElementsByClassName("nav-link").style.display = "block";
+  }
+})()
+
+
+  (() => {
+    window.onscroll = () => addOrRemoveSticky();
+
+    const navbar = document.getElementById("navbar");
+    let sticky = navbar.offsetTop;
+
+    const addOrRemoveSticky = () => {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+  })()

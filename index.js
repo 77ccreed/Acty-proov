be0bbed22275f767ea38132f451528c1d351d29c
Kiur Jullis <kiur.jullis@gmail.com>
2022-09-11 14:05:54 +0300

    All functsions to IIFI

--- index.js
+++ index.js
@@ -16,32 +16,33 @@
   function mobileMenuClick(e) {
     e.preventDefault();
   }
+
+  //Subnav link click event
+  let elements = document.getElementsByClassName("subnav-content-link");
+  for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].onclick = function (e) {
+      e.preventDefault();
+      alert("Congratulations! You have clicked the subnav link!");
+    }
+  }
+
+
+  // When the user scrolls the page, execute function
+  window.onscroll = function () { addOrRemoveSticky() };
+
+  // Get the navbar
+  const navbar = document.getElementById("navbar");
+
+  // Get the offset position of the navbar
+  let sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  const addOrRemoveSticky = () => {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
 })();
-
-//Subnav link click event
-let elements = document.getElementsByClassName("subnav-content-link");
-for (var i = 0, len = elements.length; i < len; i++) {
-  elements[i].onclick = function (e) {
-    e.preventDefault();
-    alert("Congratulations! You have clicked the subnav link!");
-  }
-}
-
-
-// When the user scrolls the page, execute function
-window.onscroll = function () { addOrRemoveSticky() };
-
-// Get the navbar
-const navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-const addOrRemoveSticky = () => {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
